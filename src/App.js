6ec6c84cc3b80a9c17f6c0536b2ffd14abcc6523
Yuxuan <jiangy10@rose-hulman.edu>
2023-08-19 00:33:38 -0400

    current wit hbug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
 import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import {Button} from "react-bootstrap";
+import { BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
+import {Button, Container, Image, Form} from "react-bootstrap";
 import CoursePage from "./Pages/CoursePage";
 import ProfessorPage from "./Pages/ProfessorPage";
+import logo from './logo.jpg'
+import React, {useState} from "react";
 
 const router = createBrowserRouter([
     {
@@ -16,16 +17,47 @@ const router = createBrowserRouter([
 ]);
 
 export default function App() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [hrefLink,setHrefLink] = useState("/");
+
+
     return (
         <div>
-            {/*<Button href={'/CoursePage'}>Login</Button>*/}
-            <BrowserRouter>
-                <Routes>
-                    <Route path={'/CoursePage/*'} element={<CoursePage />} />
-                    <Route path={'/ProfessorPage/*'} element={<ProfessorPage/>}/>
-                </Routes>
-            </BrowserRouter>
+            <Container style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'center', height:'100vh', width:'100vw'}}>
+                <Container style={{flex:'3',backgroundColor:'#801400',width:'100vw',height:'100vh',display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'center' }}>
+                    <Image src={logo} style={{width:'300px', height: "auto"}}></Image>
+                </Container>
+                <Container style={{flex:'7',width:'100vw',height:'100vh',display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center'}}>
+                    <h1>Rose-Hulman Login</h1>
+                    <Form>
+                        <Form.Label style={{fontSize:'30px'}}>Username: </Form.Label>
+                        <Form.Control
+                            style={{fontSize:'25px'}}
+                            onChange={e=>(setUsername(e.target.value))}
+                            value={username}
+                        ></Form.Control>
+                    </Form>
+                    <Form style={{marginTop:'10px'}}>
+                        <Form.Label style={{fontSize:'30px'}}>Password:</Form.Label>
+                        <Form.Control
+                            style={{fontSize:'25px'}}
+                            onChange={e=>(setPassword(e.target.value))}
+                            value={password}
+                        ></Form.Control>
+                    </Form>
+                    <Button onClick={login} style={{backgroundColor:'#801400',width:'100px', height:'40px',margin:'20px',color:'white',borderRadius:'5px'}}>Log In</Button>
+                </Container>
+            </Container>
+
         </div>
     );
+
+    function login(){
+        if(username.match("admin") && password.match("admin")){
+            setHrefLink("/CoursePage/")
+            console.log({hrefLink})
+        }
+    }
 
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
+import CoursePage from "./Pages/CoursePage";
+import ProfessorPage from "./Pages/ProfessorPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/*"} element={<App/>}/>
+              <Route path={'/CoursePage/*'} element={<CoursePage />} />
+              <Route path={'/ProfessorPage/*'} element={<ProfessorPage/>}/>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
